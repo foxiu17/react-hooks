c67916aa4cc57da3d028b67830fcead81e3402ae
Marcin Foks <mfoks@softfusion.pl>
2019-06-13 14:40:44 +0200

    hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 import UploadForm from './components/UploadForm/UploadForm';
 
@@ -7,14 +7,35 @@ import { Section, Content, Container } from './App.style';
 import ImagesList from './components/ImagesList/ImagesList';
 
 const App = () => {
+
+  const [images, setImages ] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  }
+
+  const handleImageSend = (event) => {
+    event.preventDefault();
+
+    console.log(event.target.files[0]);
+
+    let readFile = new FileReader();
+    let file = event.target.files[0];
+
+    readFile.onloadend = () => {
+      setImages([...images, { file: file, url: readFile.result}]);
+    };
+
+    readFile.readAsDataURL(file);
+  }
   return (
     <Fragment>
       <GlobalStyle />
       <Section>
         <Content>
           <Container>
-            <UploadForm />
-            <ImagesList />
+            <UploadForm handleSubmit={handleSubmit} handleImageSend={handleImageSend} />
+            <ImagesList images={images} />
           </Container>
         </Content>
       </Section>
--- src/components/UploadForm/UploadForm.js
+++ src/components/UploadForm/UploadForm.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 import { Content, Headline, Submit, Input } from './UploadForm.style';
 
-const UploadForm = () => {
+const UploadForm = ({handleSubmit, handleImageSend}) => {
   return (
     <Content>
       <Headline>UPLOAD YOUR IMAGES</Headline>
-      <form>
-        <Input type="file" />
-        <Submit type="button">Upload</Submit>
+      <form onSubmit={(event) => handleSubmit(event)}>
+        <Input type="file" onChange={(event) => handleImageSend(event)} />
+        <Submit type="submit" onClick={(event) => handleSubmit(event)} >Upload</Submit>
       </form>
     </Content>
   );
