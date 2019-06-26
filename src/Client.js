74545f91640eafac3d662bd1c59f2aeb1702a6d5
Marcin Foks <mfoks@softfusion.pl>
2019-06-26 23:15:15 +0200

    ..

--- src/Client.js
+++ src/Client.js
@@ -2,6 +2,6 @@ import ApolloClient from 'apollo-boost';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 export const client = new ApolloClient({
-  uri: 'http://localhost:8000/api',
+  uri: 'https://image-cloud-server.herokuapp.com/',
   cache: new InMemoryCache()
 });
